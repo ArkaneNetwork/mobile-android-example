86b1fcdf2a825984f7541ee997dd168e8857bca1
Nico Peeters <nico@itwhiz.be>
2019-02-07 15:51:13 +0100

    Add status to callback Intent

--- app/src/main/java/network/arkane/arketype/ArkaneCallbackActivity.java
+++ app/src/main/java/network/arkane/arketype/ArkaneCallbackActivity.java
@@ -3,12 +3,25 @@ package network.arkane.arketype;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ArkaneCallbackActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceBundle) {
         super.onCreate(savedInstanceBundle);
+
+        if (getIntent().getData() != null) {
+            final String status = getIntent().getData().getQueryParameter("status");
+            if ("success".equalsIgnoreCase(status)) {
+                Toast.makeText(getApplicationContext(), "You successfully linked wallets to this application", Toast.LENGTH_LONG)
+                        .show();
+            } else if ("aborted".equalsIgnoreCase(status)) {
+                Toast.makeText(getApplicationContext(), "You did not change the wallets linked to this application", Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+
         Intent intent = new Intent(this, TokenActivity.class);
         startActivity(intent);
         finish();
